0bc49a9be457b273feff2e1751bcd0697e0e406f
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-23 15:11:07 -0500

    added readMe and enginner.js

---
+++ 10-template-engine/lib/Engineer.js
@@ -0,0 +1,35 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+class Engineer{
+    constructor (name, id, email,github) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.github = github;
+    }
+
+    getGitHub(){
+        return this.github;
+
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return "Engineer"
+    }
+
+}
+
+module.exports = Engineer;
